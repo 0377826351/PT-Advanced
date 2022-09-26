704d254ac1314ea0eca7cf469fc76fc659e66b02
cumingg <nguyentuanminh3120@gmail.com>
2022-09-26 13:21:22 +0700

    bt-26/9

--- asigm_1_proj/asigm_1_app/urls.py
+++ asigm_1_proj/asigm_1_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('index', views.index, name='index'),
     path('detail/<int:id>', views.detail, name='detail'),
+    path('register', views.register, name='register'),
 ]
--- asigm_1_proj/asigm_1_app/views.py
+++ asigm_1_proj/asigm_1_app/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Person
 from django.template import loader
+from .forms import RegisterForm
+from django.contrib.auth.models import User
 
 def index(request):
     person_list = Person.objects.all()
@@ -19,3 +21,18 @@ def detail(request,id):
         'person':person,
     }
     return HttpResponse(template.render(context,request))
+
+def register(request):
+    if request.method == 'POST':
+        response = HttpResponse()
+
+        new_user = User(username=request.POST['username'],email=request.POST['email'],password=request.POST['password'])
+        new_user.save()
+
+        response.write("<h1>Thanks for RES</h1></br>")
+        response.write("Your username:" + request.POST['username'] + "</br>")
+        response.write("Your email:" + request.POST['email'] + "</br>")
+        return response
+
+    registerForm = RegisterForm()
+    return render(request,'register.html',{'form':registerForm})
