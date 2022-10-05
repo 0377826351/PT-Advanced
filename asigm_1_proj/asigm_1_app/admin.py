28b9d43f2b7fd8e5475909dfc63663189b4086f8
cumingg <nguyentuanminh3120@gmail.com>
2022-10-05 08:08:36 +0700

    bt 5/10

--- asigm_1_proj/asigm_1_app/admin.py
+++ asigm_1_proj/asigm_1_app/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Person
+from .models import Person,Personn,Car,Manufacturer
 
 admin.site.register(Person)
+admin.site.register(Personn)
+admin.site.register(Car)
+admin.site.register(Manufacturer)
--- asigm_1_proj/asigm_1_app/models.py
+++ asigm_1_proj/asigm_1_app/models.py
@@ -10,3 +10,21 @@ class Person(models.Model):
     mobile_number = models.IntegerField()
     def __str__(self):
         return self.name
+
+class Personn(models.Model):
+    id = models.AutoField(primary_key=True),
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    def __str__(self):
+        return self.name
+
+class Manufacturer(models.Model):
+    name_manu = models.CharField(max_length=255,default='SOMESTRING')
+    def __str__(self):
+        return self.name_manu
+
+class Car(models.Model):
+    manufacturer = models.ForeignKey(Manufacturer,on_delete=models.CASCADE)
+    name_car = models.CharField(max_length=255,default=None)
+    def __str__(self):
+        return self.name_car
